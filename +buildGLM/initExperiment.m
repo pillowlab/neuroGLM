d46fd33e03a6fff2e9f3a7c47eed022f2f5f0372
Il Memming Park <memming@gmail.com>
2015-01-13 09:51:10 -0500

    Unique ID added to experiment

--- +buildGLM/initExperiment.m
+++ +buildGLM/initExperiment.m
@@ -1,6 +1,11 @@
-function expt = initExperiment(unitOfTime, binSize)
+function expt = initExperiment(unitOfTime, binSize, uniqueID)
 % Initialize the Experiment structure that holds raw data
-% expt = initExperiment(unitOfTime, binSize)
+% expt = initExperiment(unitOfTime, binSize, uniqueID)
+%
+%   unitOfTime: 'string' - 's' or 'ms' indicating a global unit of time
+%   binSize: [1] - duration of each time bin in units of unitOfTime
+%   uniqueID: 'string' optional - Unique identifier for this experiment
+%       a compact string for easy future reference
 
 assert(binSize > 0);
 assert(ischar(unitOfTime), 'Put a string for unit');
@@ -9,3 +14,7 @@ expt = struct('unitOfTime', unitOfTime, 'binSize', binSize);
 expt.meta.type = struct();
 expt.meta.desc = struct();
 expt.meta.dim = struct();
+
+if nargin > 2
+    expt.id = uniqueID;
+end

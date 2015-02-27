bc0698f5488cba27938327a2340a13e95c172d5e
Jacob Yates <jacobyates@LA-CPS828325MN-Huk.local>
2015-02-27 10:10:24 -0600

    compileDenseDesignMatrix has massive speedup over sparse version

---
+++ +buildGLM/compileDenseDesignMatrix.m
@@ -0,0 +1,42 @@
+function dm = compileDenseDesignMatrix(dspec, trialIndices)
+% Compile information from experiment according to given DesignSpec
+
+expt = dspec.expt;
+subIdxs = buildGLM.getGroupIndicesFromDesignSpec(dspec);
+
+trialT = ceil([expt.trial(trialIndices).duration]/expt.binSize);
+totalT = sum(trialT);
+X      = zeros(totalT, dspec.edim);
+
+trialIndices = trialIndices(:)';
+
+for kTrial = trialIndices
+    ndx = sum(trialT(1:kTrial))-(trialT(kTrial)-1):sum(trialT(1:kTrial));
+        
+    for kCov = 1:numel(dspec.covar) % for each covariate
+        covar = dspec.covar(kCov);
+        sidx = subIdxs{kCov};
+        
+        if isfield(covar, 'cond') && ~isempty(covar.cond) && ~covar.cond(expt.trial(kTrial))
+            continue;
+        end
+        
+        stim = covar.stim(expt.trial(kTrial), trialT(kTrial)); % either dense or sparse
+        stim = full(stim);
+        
+        if isfield(covar, 'basis') && ~isempty(covar.basis)
+            X(ndx, sidx) = basisFactory.convBasis(stim, covar.basis, covar.offset);
+        else
+            X(ndx, sidx) = stim;
+        end
+    end
+end
+
+dm.X = X;
+dm.trialIndices = trialIndices;
+dm.dspec = dspec;
+
+%% Check sanity of the design
+if any(~isfinite(dm.X(:)))
+    warning('Design matrix contains NaN or Inf...this is not good!');
+end
